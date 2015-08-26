b5d2750faec13c00fd6e25a817811928d1ab1462
metrakit <contact@jordane.net>
2015-08-26 19:52:34 +0200

    bugfix seeder

--- seeds/NavigationsTableSeeder.php
+++ seeds/NavigationsTableSeeder.php
@@ -2,26 +2,26 @@
 
 namespace Minetop;
 
-class NavigationsTableSeeder extends Seeder {
+class NavigationsTableSeeder extends \Seeder {
 
     public function run()
     {
-        DB::table('navigations')->delete();
+        \DB::table('navigations')->delete();
 
-        $title1 = new I18n;
-        $title1->i18n_type_id = I18nType::where('name','=','title')->first()->id;
+        $title1 = new \I18n;
+        $title1->i18n_type_id = \I18nType::where('name','=','title')->first()->id;
         $title1->save();
         $title1->translate('fr','Accueil');
         $title1->translate('en','Home');
 
-        $title7 = new I18n;
-        $title7->i18n_type_id = I18nType::where('name','=','title')->first()->id;
+        $title7 = new \I18n;
+        $title7->i18n_type_id = \I18nType::where('name','=','title')->first()->id;
         $title7->save();
         $title7->translate('fr','Contact');
         $title7->translate('en','Contact');
 
 
-        DB::table('navigations')->insert( array(
+        \DB::table('navigations')->insert( array(
             array(
                 'i18n_title'        => $title1->id,
                 'parent_id'         => 0,
@@ -40,17 +40,17 @@ class NavigationsTableSeeder extends Seeder {
 
 
         //Admin navigation group
-        DB::table('admin_navigation_groups')->delete();
+        \DB::table('admin_navigation_groups')->delete();
 
-        $adminNav1 = I18n::add(array('fr'=>'Racine','en'=>'Root'), 'key', 'admin.admin_navigation_root');
-        $adminNav2 = I18n::add(array('fr'=>'Gestion des contenus','en'=>'Content managment'), 'key', 'admin.admin_navigation_content');
-        $adminNav3 = I18n::add(array('fr'=>'Gestion des utilisateurs','en'=>'User managment'), 'key', 'admin.admin_navigation_user');
-        $adminNav4 = I18n::add(array('fr'=>'Traduction','en'=>'Translation'), 'key', 'admin.admin_navigation_translation');
-        $adminNav5 = I18n::add(array('fr'=>'Option','en'=>'Option'), 'key', 'admin.admin_navigation_option');
-        $adminNav6 = I18n::add(array('fr'=>'Gestion des tops','en'=>'Top managment'), 'key', 'admin.admin_navigation_top');
+        $adminNav1 = \I18n::add(array('fr'=>'Racine','en'=>'Root'), 'key', 'admin.admin_navigation_root');
+        $adminNav2 = \I18n::add(array('fr'=>'Gestion des contenus','en'=>'Content managment'), 'key', 'admin.admin_navigation_content');
+        $adminNav3 = \I18n::add(array('fr'=>'Gestion des utilisateurs','en'=>'User managment'), 'key', 'admin.admin_navigation_user');
+        $adminNav4 = \I18n::add(array('fr'=>'Traduction','en'=>'Translation'), 'key', 'admin.admin_navigation_translation');
+        $adminNav5 = \I18n::add(array('fr'=>'Option','en'=>'Option'), 'key', 'admin.admin_navigation_option');
+        $adminNav6 = \I18n::add(array('fr'=>'Gestion des tops','en'=>'Top managment'), 'key', 'admin.admin_navigation_top');
 
 
-        DB::table('admin_navigation_groups')->insert( array(
+        \DB::table('admin_navigation_groups')->insert( array(
             array(
                 'i18n_title'        => $adminNav1,
                 'parent_id'         => 0,
